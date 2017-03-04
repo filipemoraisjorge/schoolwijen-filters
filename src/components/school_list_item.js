6b8dae58cc85f205b4dcb00a44638ec8cd6975dd
Filipe Jorge <filipemoraisjorge@gmail.com>
2017-03-04 20:57:04 +0100

    style: smaller list

--- src/components/school_list_item.js
+++ src/components/school_list_item.js
@@ -4,15 +4,8 @@ const SchoolListItem = ({school, onSchoolSelect}) => {
   //const imageUrl = school.snippet.thumbnails.default.url;
 
   return (
-    <li onClick={() => onSchoolSelect(school)} className="list-group-item">
-      <div className="school-list media">
-        <div className="media-left">
-        </div>
-        <div className="media-body">
-          <div className="media-heading">{school.id}</div>
-
-        </div>
-      </div>
+    <li style={{fontSize: 8+'pt', height: 1+'px'}} onClick={() => onSchoolSelect(school)} className="list-group-item">
+     {school.id}
     </li>
   );
 };
